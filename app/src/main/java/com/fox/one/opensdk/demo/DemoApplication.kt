d6570ab00f16cb97a5a130d0695b403e99f25d03
firefly1126 <xiaoming1109@gmail.com>
2018-09-04 20:34:12 +0800

    add reference com.fox.one:opensdk:0.0.2

---
+++ app/src/main/java/com/fox/one/opensdk/demo/DemoApplication.kt
@@ -0,0 +1,19 @@
+package com.fox.one.opensdk.demo
+
+import android.app.Application
+import com.fox.one.opensdk.FoxOneOpenSDK
+
+/**
+ * class description here
+ * @author xiaoming1109@gmail.com
+ * @version 1.0.0
+ * @since 2018-09-04
+ */
+class DemoApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        FoxOneOpenSDK.init(this)
+    }
+}
